95f8e8798bfb648909cf88db7cbe2f1688743cef
baixuejiao <baixuejiao.520@163.com>
2020-02-07 14:40:25 +0800

    webpack

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,3 +1,5 @@
+// 导入样式文件
+import './index.css'
 
 let a = 123;
 console.log(123);
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path'); //导入path模块
 const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   // 入口文件
@@ -12,6 +13,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     // 出口文件的文件名
     filename: '[name].js', // 与入口文件同名
+    publicPath: 'http://localhost:8082/'
   },
   // 插件配置
   plugins: [
@@ -27,7 +29,9 @@ module.exports = {
       },
       // 给js文件加一个版本号，为了避免服务器的缓存
       hash: true
-    })
+    }),
+    // css文件输出的路径
+    new ExtractTextPlugin('css/index.css')
   ],
   // 配置服务于热更新（配置webpack开发服务功能）
   devServer: {
@@ -39,5 +43,29 @@ module.exports = {
     port: 8082,
     // 自动打开浏览器
     open: true
+  },
+  module: {
+    rules: [
+      {
+        // 正则表达式 匹配以.css结尾的文件
+        test: /\.css$/,
+        // use: ['style-loader', 'css-loader']
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      }, {
+        loader: 'url-loader',
+        test: /\.(png|jpg|gif)$/,
+        options: {
+          limit: 500,
+          outputPath: 'images/',
+          esModule: false
+        }
+      }, {
+        test: /\.(htm|html)$/i,
+        loader: 'html-withimg-loader'
+      }
+    ]
   }
 }
